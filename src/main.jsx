e55e40725dfe215ed75d3f4d23bc02897473517f
Alla1965 <savanna.opt@gmail.com>
2025-04-27 13:49:54 +0300

    fix components

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store'; 
 import { PersistGate } from 'redux-persist/integration/react';
-import App from './Components/App';
+import App from './components/App';
 
 
 //Рендеринг React-додатка:
